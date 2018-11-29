0ba0196b5b9f03c41b00a50ee5eb8001d8f72a17
hmshend <shen11huiming@126.com>
2018-11-29 22:25:28 +0800

    合并apps

--- MyBlog/MyBlog/settings.py
+++ MyBlog/MyBlog/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
-    'news',
-    'doc',
-    'course',
+    'apps.users',
+    'apps.news',
+    'apps.doc',
+    'apps.course',
 ]
 
 MIDDLEWARE = [
--- MyBlog/apps/doc/apps.py
+++ MyBlog/apps/doc/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class DocConfig(AppConfig):
-    name = 'doc'
+    name = 'apps.doc'
