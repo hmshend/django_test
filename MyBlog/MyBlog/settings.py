9ac550e1260149c5bd671b4d458721e8a344bddb
hmshend <shen11huiming@126.com>
2018-12-03 22:35:32 +0800

    用户表设计

--- MyBlog/MyBlog/settings.py
+++ MyBlog/MyBlog/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import logging
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# 这里需要修改
+sys.path.insert(0, BASE_DIR)
+sys.path.insert(1, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -38,10 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.users',
-    'apps.news',
-    'apps.doc',
-    'apps.course',
+    # 'apps.users',
+    'news',
+    'doc',
+    'course',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -168,6 +172,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'users.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
--- MyBlog/apps/users/models.py
+++ MyBlog/apps/users/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser, UserManager as _UserManager
 # Create your models here.
+
+class UserManager(_UserManager):
+    """
+
+    """
+    def create_superuser(self, username, password, email=None, **extra_fields):
+        super(UserManager, self).create_superuser(username=username,
+                                                  password=password,
+                                                  email=email,
+                                                  **extra_fields)
+
+
+class User(AbstractUser):
+    """
+    add mobile、email_active fields to Django users modules.
+    """
+    REQUIRED_FIELDS = ['mobile']
+    object = UserManager()
+    mobile = models.CharField(max_length=11, unique=True, help_text="手机号", verbose_name="手机号",
+                              error_messages={
+                                  'unique': "此手机号已经注册"
+                              })
+    email_active = models.BooleanField(default=False, verbose_name='邮箱验证状态')
+    class Meta:
+        db_table = 'tb_users'  #表名字
+        verbose_name = '用户'  #admin站点显示中文信息
+        verbose_name_plural = verbose_name  #复数形式
+
+    def __str__(self):
+        return self.username
+
